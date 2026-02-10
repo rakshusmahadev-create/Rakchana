e7aa7022cede105185c15a1cd07b690784676471
Rakchana <rakshusmahadev@gmail.com>
2026-02-10 11:12:31 +0530

    Practice questions.java

---
+++ Practice questions.java
@@ -0,0 +1,73 @@
+1.Get the input for all data types and print the values
+(byte,short,int,float,long,double,char,string)
+program:
+import java.util.*;
+public class Main
+{
+     public static void main (String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        byte a = scan.nextByte();
+        short b = scan.nextByte();
+        int c = scan.nextInt();
+        float d = scan.nextFloat();
+        long e = scan.nextLong();
+        double f = scan.nextDouble();
+        String str1 = scan.next();
+        scan.nextLine();//getting string input between some values
+        String str2 = scan.nextLine();
+        char ch = scan.next().charAt(0);
+        System.out.println(a+" "+b);
+        System.out.println(c+" "+d);
+        System.out.println(e+" "+f);
+        System.out.println(str1+" "+str2);
+        System.out.println(ch);
+    }
+}
+o/p:
+3 12
+56 9.6
+678 89.565
+hello hello hi
+h
+  
+Program 2:(sum of the digits)
+import java.util.*;
+public class Main
+{
+     public static void main (String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int sum = 0;
+        while(a>0) {
+          int r = a%10;
+          sum = sum+r;
+          a=a/10;
+        }
+        System.out.println(sum);
+    }
+}
+input:456
+o/p:15
+  
+Program 3(Reverse the digit):
+import java.util.*;
+public class Main
+{
+     public static void main (String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int rev = 0;
+        while(a>0) {
+          int r = a%10;
+          rev=rev*10+r;
+          a=a/10;
+        }
+        System.out.println(rev);
+    }
+}
+Input: 456
+o/p:
+654
