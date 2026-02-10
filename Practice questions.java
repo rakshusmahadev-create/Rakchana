3e5d9e45b78259d9c71360b51e9a94d3193129f7
Rakchana <rakshusmahadev@gmail.com>
2026-02-10 11:34:17 +0530

    Practice questions.java

--- Practice questions.java
+++ Practice questions.java
@@ -71,3 +71,31 @@ public class Main
 Input: 456
 o/p:
 654
+program 4(palindrome)
+     import java.util.*;
+public class Main
+{
+     public static void main (String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int rev = 0;
+        int temp=a;
+        while(a>0) {
+          int r = a%10;
+          rev=rev*10+r;
+          a=a/10;
+        }
+        if(rev==temp){
+          System.out.println("Palindrome");
+        }
+        else{
+          System.out.println("Not Palindrome");
+        }
+        System.out.println(rev);
+    }
+}
+input:677
+output:
+Not Palindrome
+776
