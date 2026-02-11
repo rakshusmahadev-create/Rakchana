3364e8bdbe892ff446ddb91167f461cfff807ce3
Rakchana <rakshusmahadev@gmail.com>
2026-02-11 16:22:10 +0530

    Practice question 2.java

--- Practice question 2.java
+++ Practice question 2.java
@@ -118,3 +118,95 @@ public class Main {
 2 3 4 5
   o/p:
 Maximum number = 5
+
+program(5)(Max number finding)
+    import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int size = sc.nextInt();
+      int arr[] = new int[size];
+      for(int i=0;i<size;i++){
+        arr[i]=sc.nextInt();
+      }
+      int max = arr[0];
+      for(int i=1;i<size;i++){
+        if(arr[i]>max){
+          max=arr[i];
+        }
+      }
+      System.out.println(max);
+    }
+}
+i/p:4
+45 67 23 12
+o/p:
+67
+program(6)(min number finding)
+    import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for(int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];   
+
+        for(int i = 1; i < size; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println(min);
+    }
+}
+i/p:
+4
+45 67 23 12
+o/p:
+12
+
+Program(7)(second max finding)
+    import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for(int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for(int i = 0; i < size; i++) {
+
+            if(arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            }
+            else if(arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
+            }
+        }
+
+        System.out.println("Second Maximum = " + secondMax);
+    }
+}
+i/p:4
+45 67 23 12
+o/p:
+Second Maximum = 45
