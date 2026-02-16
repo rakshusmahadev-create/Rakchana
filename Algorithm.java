5ef444406cb02b721d352aa3f2d850fc6a45d252
Rakchana <rakshusmahadev@gmail.com>
2026-02-16 16:06:50 +0530

    Algorithm.java

--- Algorithm.java
+++ Algorithm.java
@@ -114,3 +114,27 @@ public class Main {
       System.out.println(Arrays.toString(arr));
     }
 }
+
+//Insertion Sorting
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int arr[]=new int[n];
+      for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+      }
+      for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j]>key){
+          arr[j+1]=arr[j];
+          j--;
+        }
+        arr[j+1]=key;
+      }
+      System.out.println(Arrays.toString(arr));
+    }
+}
