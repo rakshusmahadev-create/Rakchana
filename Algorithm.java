c0f92990ae70bec7adb2fc89e9afc9c8171db1bb
Rakchana <rakshusmahadev@gmail.com>
2026-02-16 14:34:36 +0530

    Algorithm.java

--- Algorithm.java
+++ Algorithm.java
@@ -88,3 +88,29 @@ public class Main {
       System.out.println(Arrays.toString(arr));
     }
 }
+
+//Selection Sorting
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int arr[] = new int[n];
+      for(int i = 0;i<n;i++){
+        arr[i]=sc.nextInt();
+      }
+      for(int i=0;i<n;i++){
+        int minindex=i;
+        for(int j = i+1;j<n;j++){
+          if(arr[j]<arr[minindex]){
+            minindex=j;
+          }
+        }
+        int temp = arr[i];
+        arr[i]=arr[minindex];
+        arr[minindex]=temp;
+      }
+      System.out.println(Arrays.toString(arr));
+    }
+}
