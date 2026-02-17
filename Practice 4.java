32d6e6b68ac47d348fbd3bd14b6c25d82e296ad9
Rakchana <rakshusmahadev@gmail.com>
2026-02-17 15:17:01 +0530

    Practice 4.java

--- Practice 4.java
+++ Practice 4.java
@@ -41,3 +41,26 @@ public class Main{
     m2.print();
   }
 }
+//initialization through reference
+class Student{
+    int reg;
+    String name;
+    }
+
+public class Main
+{
+	public static void main(String[] args) {
+		Student m1= new Student();
+		m1.reg=101;
+		m1.name="ashwin";
+
+		Student m2=new Student();
+		m2.reg=102;
+		m2.name="baskaran";
+		System.out.println(m1.reg);
+		System.out.println(m1.name);
+		System.out.println(m2.reg);
+		System.out.println(m2.name);
+
+	}
+}
