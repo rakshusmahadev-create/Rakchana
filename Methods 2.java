249461d8cc109042c28389424edeb4ec362093c6
Rakchana <rakshusmahadev@gmail.com>
2026-02-17 10:42:42 +0530

    Methods 2.java

--- Methods 2.java
+++ Methods 2.java
@@ -28,3 +28,36 @@ public class Main {
    
     }
 }
+
+Program 2//with arg without return type (non static)
+
+import java.util.*;
+
+public class Main {
+    
+      public void prime(int a){
+        int count=0;
+        for(int i=1;i<=a;i++){
+          if(a%i==0){
+            count++;
+          }
+          
+        }
+        if(count==2){
+          System.out.println(true);
+        }
+        else{
+          System.out.println(false);
+        }
+      }
+      public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int num1 = sc.nextInt();
+        Main m = new Main();
+        m.prime(num);
+        m.prime(num1);
+      
+   
+    }
+}
