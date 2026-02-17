e3a2b77a49e1e7f074bc646d9bcd69224f8fe1b9
Rakchana <rakshusmahadev@gmail.com>
2026-02-17 14:19:03 +0530

    Practice 4.java

---
+++ Practice 4.java
@@ -0,0 +1,22 @@
+//Intialization through methods
+class Student{
+  int reg;
+  String name;
+  void assign(int r,String n) {
+    reg = r;
+    name = n;
+  }
+  void print(){
+    System.out.println(reg + " "+name);
+  }
+}
+public class Main{
+  public static void main(String[] args){
+    Student m1 = new Student();
+    m1.assign(101,"arun");
+    Student m2 = new Student();
+    m2.assign(102,"karthick");
+    m1.print();
+    m2.print();
+  }
+}
