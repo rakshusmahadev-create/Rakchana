89abaf528fc31c60012e7f6f7d26522bb1c21c2d
Rakchana <rakshusmahadev@gmail.com>
2026-02-18 12:30:56 +0530

    Data structure.java

--- Data structure.java
+++ Data structure.java
@@ -63,3 +63,59 @@ public class Main {
      s.display();
     }
 }
+//program 3:
+class Node{
+  int data;
+  Node next;
+  Node(int d){
+    data=d;
+    next=null;
+  }
+}
+class singlylinkedlist{
+  Node head = null;
+  Node tail= null;
+  void add(int data){
+    Node newnode = new Node(data);
+    if(head==null){
+      head=tail=newnode;
+    }
+    else{
+      tail.next=newnode;
+      tail=newnode;
+    }
+  }
+  void addatbeg(int data){
+    Node newnode = new Node(data);
+    if(head==null){
+      head = tail = newnode;
+    }
+    else{
+      newnode.next=head;
+      head=newnode;
+    }
+  }
+  void display(){
+    Node temp = head;
+    while(temp!=null){
+      System.out.print(temp.data+" ");
+      temp = temp.next;
+    }
+  }
+}
+
+public class Main {
+    public static void main(String[] args) {
+     singlylinkedlist s = new singlylinkedlist();
+     s.add(100);
+     s.display();
+     s.add(200);
+     s.display();
+     s.add(300);
+     s.display();
+     s.addatbeg(400);
+     s.display();
+    }
+}
+//Program 4:
+https://www.onlinegdb.com/CLuwbVthk -- 
